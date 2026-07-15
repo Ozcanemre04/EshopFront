4d1f2a05236acc86c7d0bf562aa346a9b3d49f04
Emre Ozcan <ozcan.emre1608@gmail.com>
2026-07-15 17:41:55 +0200

    environment folder added

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:false,
+    apiUrl:'http://localhost:5010',
+    publishableKey:'pk_test_51Rn0AAGh7j8lO4f9Kv3myWCz9XIGtzGwlEwBYwidNsFC9JwF9rZqi5xlyFEM2DxD3dTwYifoiC6E4x425tS1mJhH00xJiLFblq'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:true,
+    apiUrl:'https://eshopapi-qx3h.onrender.com',
+    publishableKey:'pk_test_51Rn0AAGh7j8lO4f9Kv3myWCz9XIGtzGwlEwBYwidNsFC9JwF9rZqi5xlyFEM2DxD3dTwYifoiC6E4x425tS1mJhH00xJiLFblq'
+};
